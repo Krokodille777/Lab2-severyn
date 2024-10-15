c0c7d66b69014c5c4eea35e387eb39b3fe5652d3
Krokodille777 <krokocoder123@gmail.com>
2024-10-15 16:40:27 +0300

    All bugs were fixed. System works well.  Excluding <string> library

--- project.cpp
+++ project.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 int main(){
     int boxesWithChocolates = 20, LoadedBoxes, carsN, remainedBoxes;
@@ -8,12 +7,17 @@ int main(){
     for (int cars = 1; cars <= carsN; cars++){
         std::cout<<"Enter the number of boxes that should be loaded: ";
         std::cin>>LoadedBoxes;
-        remainedBoxes = boxesWithChocolates - LoadedBoxes;
-        boxesWithChocolates = remainedBoxes;
-        if (LoadedBoxes > remainedBoxes){
+        if (LoadedBoxes > boxesWithChocolates){
             std::cout<<"There is no enough boxes to load"<<std::endl;
-
+            
         }
+        else {
+            remainedBoxes = boxesWithChocolates - LoadedBoxes;
+            boxesWithChocolates = remainedBoxes;
+            }
+
+        
+        
         if (boxesWithChocolates == 0 && cars != carsN){
             std::cout<<"There is no more boxes to load. Not all cars were loaded."<<std::endl;
             break;
@@ -21,6 +25,9 @@ int main(){
         else if (boxesWithChocolates == 0 && cars == carsN){
             std::cout<<"There is no more boxes to load. All cars were loaded."<<std::endl;
             break;
+        }
+        else if (boxesWithChocolates != 0 && cars == carsN){
+            std::cout<<"We need more cars!!!"<<std::endl;
         }
         else{
             std::cout<<"There is "<<remainedBoxes<<" boxes left to load."<<std::endl;
